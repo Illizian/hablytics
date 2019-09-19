02547dc1321822d8239187a7e962edc93b393c30
Alex Scotton <alex@alexscotton.com>
2019-09-19 22:21:17 +0100

    Adds theme-color meta tag

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,6 +4,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <!-- PWA -->
+    <meta name="theme-color" content="#4299E1">
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
