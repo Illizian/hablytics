78ded7f8bc7e50d29cc4fc19929fe9086639dd73
Alex Scotton <alex@alexscotton.com>
2019-09-26 12:54:10 +0100

    Adds required missing middleware

--- app/Http/Controllers/DiaryTagController.php
+++ app/Http/Controllers/DiaryTagController.php
@@ -9,6 +9,16 @@ use App\DiaryTag;
 
 class DiaryTagController extends Controller
 {
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware(['auth', 'approved']);
+    }
+
     public function view($id)
     {
         $user = Auth::user();
